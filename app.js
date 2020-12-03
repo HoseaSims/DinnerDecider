f5f4a3cd359441256ba4f2afdfd31812c7773663
Hosea <hoseasims21@gmail.com>
2020-12-02 21:04:03 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const optionOne = document.querySelector("#option1")
+const optionTwo = document.querySelector("#option2")
+const optionThree = document.querySelector("#option3")
+const result = document.querySelector("#result")
+const form = document.querySelector("form");
+
+
+let finalValue;
+function getValue(){
+    finalValue = Math.floor(Math.random() * 3);
+    console.log(finalValue);
+}
+
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    console.log("button is reading you");
+    
+    getValue();
+    
+    if (finalValue === 0){
+        result.textContent = optionOne.value;
+        console.log(optionOne.value);
+    }
+    else if (finalValue === 1){
+        result.textContent = optionTwo.value;
+        console.log(optionTwo.value);
+    }else if (finalValue === 2){
+        result.textContent = optionThree.value;
+        console.log(optionThree.value);
+    }
+    
+    result.classList.add("text-success")
+})
